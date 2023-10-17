62569a632933fd27b969d1e513ca5e767705cd69
Crynowecer0 <crynowecer11@gmail.com>
2023-10-17 12:52:29 -0400

    feat: add links
    add Link components to index.js <->first-post
    add Link component to index.sj <->lolz

--- pages/posts/lulz/lolz.js
+++ pages/posts/lulz/lolz.js
@@ -1,3 +1,12 @@
-export default function Lulz() {
-  return <h1>for the lulz</h1>;
+import Link from 'next/link';
+
+export default function Lolz() {
+  return (
+    <>
+      <h1>for the lulz</h1>
+      <h2>
+        <Link href="/">Back to home</Link>
+      </h2>
+    </>
+  );
 }
