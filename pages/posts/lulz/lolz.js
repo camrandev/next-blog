27445def989d7c80df9134222731b4ce66e62b99
Crynowecer0 <crynowecer11@gmail.com>
2023-10-18 18:29:12 -0400

    feat: multiple non-breaking changes
    -public/images directory
    -add titles to first-post.js and lolz.js

--- pages/posts/lulz/lolz.js
+++ pages/posts/lulz/lolz.js
@@ -1,8 +1,12 @@
 import Link from 'next/link';
+import Head from 'next/head';
 
 export default function Lolz() {
   return (
     <>
+    <Head>
+      <title>for the lolz</title>
+    </Head>
       <h1>for the lulz</h1>
       <h2>
         <Link href="/">Back to home</Link>
